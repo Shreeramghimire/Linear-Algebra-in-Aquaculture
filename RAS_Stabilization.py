19b2036f0cafbcf037995ba76adb8d2c480de65d
Shreeram Ghimire <ghimireshreeram25@gmail.com>
2026-09-07 16:50:55 +0200

    Update RAS_Stabilization.py

--- RAS_Stabilization.py
+++ RAS_Stabilization.py
@@ -1,5 +1,5 @@
 # RAS Stabilization Prediction Model: 
-   It predicts biofilter/nitrogen-cycle stabilization time in a Recirculating Aquaculture System (RAS) using:
+It predicts biofilter/nitrogen-cycle stabilization time in a Recirculating Aquaculture System (RAS) using:
   1. A Monod-kinetics ODE system (ammonia -> nitrite -> nitrate via AOB/NOB)
   2. Monte Carlo simulation over uncertain initial conditions / parameters
   3. Local stability analysis via the Jacobian's eigenvalues
